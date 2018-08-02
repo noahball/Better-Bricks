3e3f5e6123ee26403777821dfb5a2a88aff441f6
Noah Ball <37968093+CyberKingg18@users.noreply.github.com>
2018-08-02 17:24:15 +1200

    Updated textures to be more advanced and have a hue. Version 1.1.

--- src/main/java/com/KiwiGeek10/betterbricks/util/Reference.java
+++ src/main/java/com/KiwiGeek10/betterbricks/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String Mod_ID = "kg10bb";
 	public static final String NAME = "Better Bricks";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.KiwiGeek10.betterbricks.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.KiwiGeek10.betterbricks.proxy.CommmonProxy";
